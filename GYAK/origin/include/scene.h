276abd6e7a506df84758564d3983f2df0a1523e6
MartinMZan <zanmartin1998@gmail.com>
2021-03-05 16:24:33 +0100

    4. week homework

---
+++ GYAK/origin/include/scene.h
@@ -0,0 +1,52 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "camera.h"
+
+typedef struct Scene
+{
+    // NOTE: Place for model structures and texture ids.
+	float teapot_rotation;
+	float sphere_radius;
+	float ball_rotation;
+} Scene;
+
+/**
+ * Initialize the scene.
+ */
+void init_scene(Scene* scene);
+
+void update_scene(Scene* scene, double time);
+
+/**
+ * Draw the scene objects.
+ */
+void draw_scene(const Scene* scene);
+
+/**
+ * Draw the origin of the world coordinate system.
+ */
+void draw_origin();
+
+/**
+ * Draw a cone. Not working.
+ */
+//void draw_cone();
+
+/**
+ * Draw a triangle.
+ */
+void draw_triangle();
+
+/**
+ * Draw a ball.
+ */
+void draw_ball(const Scene* scene);
+
+/**
+ * Draw a chess board.
+ */
+void draw_chess_board();
+
+#endif /* SCENE_H */
+
---
+++ GYAK/origin/src/scene.c
@@ -0,0 +1,151 @@
+#include "scene.h"
+
+#include <GL/glut.h>
+
+#include <math.h>
+
+void init_scene(Scene* scene)
+{
+	scene->teapot_rotation = 0.0;
+	scene->sphere_radius = 0.0;
+	scene->ball_rotation = 0.0;
+}
+
+void update_scene(Scene* scene, double time)
+{
+	scene->teapot_rotation += 15 * time;
+	scene->sphere_radius += 0.01 * time;
+	scene->ball_rotation += 10 * time;
+}
+
+void draw_scene(const Scene* scene)
+{
+	int i;
+	int n_slices;
+	
+    draw_origin();
+	
+	glPushMatrix();
+	for (i = 0; i < 10; ++i) {
+		
+		glTranslatef(1.5, 0, 0);
+		
+		glColor3f(1 - (float)i / 10, 0.8, (float)i / 10.0);
+		glPushMatrix();
+		
+		glRotatef(scene->teapot_rotation, 0, 0, 1);
+		glRotatef(90, 1, 0, 0);
+		glTranslatef(0, 2, 0);
+		
+		glutWireTeapot(1);
+		glPopMatrix();
+	}
+	glPopMatrix();
+	
+	/*
+	n_slices = scene->sphere_radius * 50;
+	if (n_slices < 3) {
+		n_slices = 3;
+	}
+	glutWireSphere(scene->sphere_radius, n_slices, 20);
+	*/
+	
+	draw_triangle();
+	
+	draw_ball(scene);
+	
+	draw_chess_board();
+	
+}
+
+void draw_origin()
+{
+    glBegin(GL_LINES);
+
+    glColor3f(1, 0, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(1, 0, 0);
+
+    glColor3f(0, 1, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 1, 0);
+
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, 1);
+
+    glEnd();
+}
+
+/*
+void draw_cone()
+{
+	float x, y, h;
+	float phi;
+	float delta;
+	
+	glColor3f(1, 0.6, 0.6);
+	glBegin(GL_TRIANGULAR_FAN);
+	
+	h = 1;
+	delta = 0.1;
+	glVertex3f(0, 0, h);
+	phi = 0.0;
+	while (phi <= 2 * M_PI + delta) {
+		x = cos(phi);
+		y = sin(phi);
+		value = phi / 2;
+		glColor3f(phi, 0, 0);
+		glVertex3f(x, y, 0);
+		phi += delta;
+	}
+
+    glEnd();
+}
+*/
+
+void draw_triangle()
+{
+	glBegin(GL_TRIANGLES);
+	glColor3f(1, 1, 0);
+	glVertex3f(0, 0, 0);
+	glColor3f(0, 1, 1);
+	glVertex3f(1, 0, 0);
+	glColor3f(1, 0, 1);
+	glVertex3f(0, 1, 1);
+	glEnd();
+}
+
+void draw_ball(const Scene* scene)
+{
+	glPushMatrix();
+	glRotatef(scene->ball_rotation, 0, 1, 0);
+	glTranslatef(0.5, -0.5, 0.5);
+	glColor3f(0.5, 0.5, 0.5);
+	glutWireSphere(0.5, 10, 10);
+	glPopMatrix();
+}
+
+void draw_chess_board()
+{
+	int i, j;
+	double size = 0.05;
+	
+	for (i = 0 ; i<=7 ; i++) {
+		for (j = 0 ; j<=7 ; j++) {
+			glPushMatrix();
+			glTranslatef(i*size, j*size, 0);
+			
+			glBegin(GL_QUADS);
+			glColor3f((i+j)%2, (i+j)%2, (i+j)%2);
+			glVertex3f(0, 0, 0);
+			glVertex3f(0, size, 0);
+			glVertex3f(size, size, 0);
+			glVertex3f(size, 0, 0);
+			glEnd();
+			
+			glPopMatrix();
+		}
+	}
+	
+}
